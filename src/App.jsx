559b9eab923a8257771ef988bfbb889bf8c65f69
Mamunur Rashid <mrashid@eservicios.indra.es>
2024-03-17 15:48:15 +0100

    testing one more

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import * as React from "react";
-import { Button, Form, TimePicker } from "antd";
-import dayjs from "dayjs";
-
-function App() {
-  const [form] = Form.useForm();
-  const initialValues = {
-    timePicker: dayjs().subtract(1, "hour"),
-  };
-  const onFinish = (values) => {
-    console.log("Received values from form: ", values);
-  };
-  const onReset = () => {
-    form.resetFields();
-  };
-
-  const format = "HH:mm";
-
+const App = () => {
   return (
-    <Form form={form} initialValues={initialValues} onFinish={onFinish}>
-      <Form.Item
-        name="timePicker"
-        label="Time Picker"
-        rules={[{ required: true, message: "Please select time!" }]}
-      >
-        <TimePicker format={format} use12Hours={false} />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button style={{ marginLeft: "8px" }} onClick={onReset}>
-          Reset
-        </Button>
-      </Form.Item>
-    </Form>
+    <div>
+      <h1>React App</h1>
+    </div>
   );
-}
+};
 
 export default App;
